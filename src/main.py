c65c4aee4174182a601911d14ac99a8ee85b8b27
j7zd <x8bd7@proton.me>
2024-09-20 09:22:20 +0300

    made units of measurement more clear

--- src/main.py
+++ src/main.py
@@ -10,11 +10,11 @@ def update_velocity():
     a, b, input_type = get_values()
     if input_type == 0:
         velocity = average_speed(float(a), float(b))
-        velocity_var.set(str(round(ms_to_kmh(velocity), 2)) + " km/h")
+        velocity_var.set(str(round(ms_to_kmh(velocity), 2)))
         brake_distance_var.set(str(round(brake_distance(velocity, 1.2, 0.2, 0.4, 1.1, 0.9), 2)) + " m") # TODO
     elif input_type == 1:
         velocity = brake_trails_speed(float(a), float(b), 0.4, 1.1, 0.9) # TODO
-        velocity_var.set(str(round(ms_to_kmh(velocity), 2)) + " km/h")
+        velocity_var.set(str(round(ms_to_kmh(velocity), 2)))
         brake_distance_var.set(str(round(brake_distance(velocity, 1.2, 0.2, 0.4, 1.1, 0.9), 2)) + " m") # TODO
     else:
         velocity_var.set("")
@@ -38,9 +38,9 @@ def update_speed_limit():
         n_speed = kmh_to_ms(Vmax(collision_distance, 1.2, 0.2, 0.4, 1.1, 0.9)) # TODO
         if n_speed is not None:
             if n_speed > speed_limit:
-                new_speed_limit_var.set(str(ms_to_kmh(speed_limit)) + " km/h")
+                new_speed_limit_var.set(str(ms_to_kmh(speed_limit)))
             else:
-                new_speed_limit_var.set(str(ms_to_kmh(n_speed)) + " km/h")
+                new_speed_limit_var.set(str(ms_to_kmh(n_speed)))
         else:
             new_speed_limit_var.set("Error")
     except:
@@ -62,21 +62,21 @@ if __name__ == "__main__":
     the_rest = tk.Frame()
     the_rest.grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
 
-    speed_label = tk.Label(the_rest, text="Velocity: ")
+    speed_label = tk.Label(the_rest, text="Velocity (km/h): ")
     speed_label.grid(row=0, column=0)
     velocity_var = tk.StringVar()
     velocity_var.set("")
     speed_entry = tk.Entry(the_rest, textvariable=velocity_var, state="readonly")
     speed_entry.grid(row=1, column=0)
 
-    brake_distance_label = tk.Label(the_rest, text="Brake distance: ")
+    brake_distance_label = tk.Label(the_rest, text="Brake distance (m): ")
     brake_distance_label.grid(row=0, column=1)
     brake_distance_var = tk.StringVar()
     brake_distance_var.set("")
     brake_distance_entry = tk.Entry(the_rest, textvariable=brake_distance_var, state="readonly")
     brake_distance_entry.grid(row=1, column=1)
 
-    collision_distance_label = tk.Label(the_rest, text="Collision distance: ")
+    collision_distance_label = tk.Label(the_rest, text="Collision distance (m): ")
     collision_distance_label.grid(row=2, column=0)
     collision_distance_entry = tk.Entry(the_rest)
     collision_distance_entry.grid(row=3, column=0)
@@ -89,13 +89,13 @@ if __name__ == "__main__":
     does_collision_entry = tk.Entry(the_rest, textvariable=does_collision_var, state="readonly")
     does_collision_entry.grid(row=3, column=1)
 
-    speed_limit_label = tk.Label(the_rest, text="Speed limit: ")
+    speed_limit_label = tk.Label(the_rest, text="Speed limit (km/h): ")
     speed_limit_label.grid(row=4, column=0)
     speed_limit_entry = tk.Entry(the_rest)
     speed_limit_entry.grid(row=5, column=0)
     speed_limit_entry.bind('<Return>', lambda event: update_speed_limit())
 
-    new_speed_limit_label = tk.Label(the_rest, text="New speed limit: ")
+    new_speed_limit_label = tk.Label(the_rest, text="New speed limit (km/h): ")
     new_speed_limit_label.grid(row=4, column=1)
     new_speed_limit_var = tk.StringVar()
     new_speed_limit_var.set("")
